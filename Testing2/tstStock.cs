07b947e2640b2f0f2efd56113cc51e9e70e41e19
MichniaUni <162065861+MichniaUni@users.noreply.github.com>
2024-05-07 20:25:29 +0100

    Adding test unit to the Stock

---
+++ Testing2/tstStock.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class Stock
+            clsStock AnStock = new clsStock();
+            //test to see taht is there
+            Assert.IsNotNull(AnStock);
+        }
+    }
+}
