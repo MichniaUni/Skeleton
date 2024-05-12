875ba0abfa7c9799c362a1fb851ddabb43a29910
MichniaUni <162065861+MichniaUni@users.noreply.github.com>
2024-05-12 20:38:57 +0100

    Adding a Simple Presentation Layer and buttons so the user can navigate between the pages.

--- AdminSystem/StockManagementDataEntry.aspx.cs
+++ AdminSystem/StockManagementDataEntry.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStock
+        clsStock AnStock = new clsStock();
+        //capture the Itemdescription
+        AnStock.ItemDescription = txtItemDescription.Text;
+        //store the address in the session object
+        Session["AnStock"] = AnStock;              
+        //navigate to the view page
+        Response.Redirect("StockManagementViewer.aspx");
     }
 }
