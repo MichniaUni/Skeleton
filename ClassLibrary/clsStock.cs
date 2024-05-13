50c6d2db75d4a4028bc4979afc3500eee4ec8039
MichniaUni <162065861+MichniaUni@users.noreply.github.com>
2024-05-13 19:18:09 +0100

    Created the Find method, added private members to the properties, and updated the getters and setters.

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -3,13 +3,10 @@
 namespace ClassLibrary
 {
     public class clsStock
-    {
+    {        
         //private data member for the stock id property
-        private Int32 mItemID;
         private DateTime mRestockDate;
-
-
-
+        // RestockDate public property
         public DateTime RestockDate 
         {
             get 
@@ -24,8 +21,26 @@ namespace ClassLibrary
             }
         }
 
-        public bool IsActive { get; set; }
-        
+        //private data member for the stock id property
+        private Boolean mIsActive;
+        // IsActive public property
+        public bool IsActive
+        {
+            get 
+            {
+                //this line of code sends data out of the property
+                return mIsActive;
+            }
+            set 
+            {
+                //this line of code allows data into the property
+                mIsActive = value;
+            }
+        }
+
+        //private data member for the stock id property
+        private Int32 mItemID;
+        // ItemID public property
         public Int32 ItemID 
         {
             get
@@ -40,18 +55,70 @@ namespace ClassLibrary
             }
         
         }
-        
 
-        
-        public int QuantityInStock { get; set; }
-        public string ItemDescription { get; set; }
-        public decimal ItemPrice { get; set; }
+        //private data member for the stock id property
+        private Int32 mQuantityInStock;
+        // QuantityInStock public property
+        public Int32 QuantityInStock
+        {
+            get 
+            {
+                //this line of code sends data out of the property
+                return mQuantityInStock;
+            }
+            set 
+            {
+                //this line of code allows data into the property
+                mQuantityInStock = value;
+            }
+        }
+
+        //private data member for the stock id property
+        private string mItemDescription;
+        // ItemDescription public property
+        public string ItemDescription
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mItemDescription;
+            }
+            set 
+            {
+                //this line of code allows data into the property
+                mItemDescription = value;
+            }
+        }
+
+        //private data member for the stock id property
+        private Decimal mItemPrice;
+        // ItemPrice public property
+        public decimal ItemPrice
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mItemPrice;
+            }
+            set 
+            {
+                //this line of code allows data into the property
+                mItemPrice = value;
+            }
+        }
+
+
+        /****** FIND METHOD ******/
 
         public bool Find(int itemID)
         {
             //set the private data members to the test data value
             mItemID = 21;
+            mItemDescription = "RedDrone";
             mRestockDate = Convert.ToDateTime("23/12/2020");
+            mQuantityInStock = 20;
+            mItemPrice = 1.23m;
+            mIsActive = true;
             //alwayse return true
             return true;
         }
