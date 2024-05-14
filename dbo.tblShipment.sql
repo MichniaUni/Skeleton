b319657c1e19f4a2e661ad47469b27002daac35a
p2785933 <p2785933@GWHXX680WIN017W>
2024-05-14 15:03:23 +0100

    added database

---
+++ dbo.tblShipment.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblShipment] (
+    [shipmentid]        INT          IDENTITY (1, 1) NOT NULL,
+    [orderid]           INT          NULL,
+    [street]            VARCHAR (50) NULL,
+    [city]              VARCHAR (50) NULL,
+    [postcode]          VARCHAR (15) NULL,
+    [delivery_Date]     DATE         NULL,
+    [isDeliveryExpress] BIT          NULL,
+    CONSTRAINT [PK_tblShipment] PRIMARY KEY CLUSTERED ([shipmentid] ASC)
+);
+
