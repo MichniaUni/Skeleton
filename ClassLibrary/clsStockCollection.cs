8eaddbfc4af04aa976f5132d3273f2e541f274df
MichniaUni <162065861+MichniaUni@users.noreply.github.com>
2024-05-18 22:31:53 +0100

    Creating the Collection Class

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStockCollection
+    {
+        //private data member for the list
+        List<clsStock> mItemList = new List<clsStock>();
+
+        public List<clsStock> ItemList
+        {
+            get
+            {
+                //return the private data
+                return mItemList;
+            }
+            set
+            {
+                //set the private data
+                mItemList = value;
+            }
+        }
+        public int Count 
+        {
+            get
+            {
+                //return the count of the list
+                return mItemList.Count;
+            }
+            set
+            {
+
+            }
+        }
+
+        public clsStock ThisItem { get; set; }
+    }
+}
