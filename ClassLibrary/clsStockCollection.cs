dc2cb4a015d737aed45037720ae3cbd03e0f7fa5
MichniaUni <162065861+MichniaUni@users.noreply.github.com>
2024-05-20 20:55:54 +0100

    Updated the Stock Collection class

--- ClassLibrary/clsStockCollection.cs
+++ ClassLibrary/clsStockCollection.cs
@@ -1,9 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ClassLibrary
 {
     public class clsStockCollection
     {
+        public clsStockCollection()
+        {
+            //create the items of test data
+            clsStock TestItem = new clsStock();
+            //set its properties
+            TestItem.IsActive = true;
+            TestItem.ItemDescription = "RedDrones";
+            TestItem.RestockDate = DateTime.Now;
+            TestItem.QuantityInStock = 1;
+            TestItem.ItemPrice = 1.23m;
+            //add the test item to the test list
+            mItemList.Add(TestItem);
+            //re initialise the object for some new data
+            TestItem = new clsStock();
+            //set its properties
+            TestItem.IsActive = true;
+            TestItem.ItemDescription = "BlueDrones";
+            TestItem.RestockDate = DateTime.Now;
+            TestItem.QuantityInStock = 2;
+            TestItem.ItemPrice = 5.23m;
+            //add the test item to the test list
+            mItemList.Add(TestItem);
+
+        }
+        
+        
         //private data member for the list
         List<clsStock> mItemList = new List<clsStock>();
 
