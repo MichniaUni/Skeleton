8512e72254a00b008931c1c11342a16c1cbb5af8
p2786795 <p2786795@my365.dmu.ac.uk>
2024-05-28 03:30:21 +0100

    Middle layer

--- AdminSystem/SupplierManagementDataEntry.aspx.cs
+++ AdminSystem/SupplierManagementDataEntry.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -14,6 +15,26 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnadd_Click(object sender, EventArgs e)
     {
+        //create a new instance of clsSupplier
+        clsSupplier AnSupplier = new clsSupplier();
+        //capture Supplier name
+        AnSupplier.SupplierName = txtSupplierName.Text;
+        //capture SupplierID
+        AnSupplier.SupplierID = Convert.ToInt32(txtSupplierID.Text);
+        //Capture ProductionDate
+        AnSupplier.ProductionDate= Convert.ToDateTime(txtProductionDate.Text);
+        //Capture Expiry date
+        AnSupplier.ExpiryDate= Convert.ToDateTime(txtExpiryDate.Text);
+        //capture quantity
+        AnSupplier.Quantity = Convert.ToInt32(txtQuantity.Text);
+        //capture price
+        AnSupplier.Price = Convert.ToInt32(txtPrice.Text);
+        //capture Shipmentstatus
+        AnSupplier.Shipmentstatus= chkShipped.Checked;
+
+        //store the supplier in the session object
+        Session["AnSupplier"] = AnSupplier;
+        //redirect to view page
         Response.Redirect("SupplierManagementViewer.aspx");
     }
 
@@ -25,5 +46,10 @@ public partial class _1_DataEntry : System.Web.UI.Page
     protected void btnCancel_Click(object sender, EventArgs e)
     {
         Response.Redirect("SupplierManagementViewer.aspx");
+    }
+
+    protected void txtSupplierName_TextChanged(object sender, EventArgs e)
+    {
+
     }
 }
--- AdminSystem/SupplierManagementViewer.aspx.cs
+++ AdminSystem/SupplierManagementViewer.aspx.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create new instance of clsSupplier
+        clsSupplier AnSupplier = new clsSupplier();
+        //get the data from yhe session object
+        AnSupplier = (clsSupplier)Session["AnSupplier"];
+        //display supplier ID
+        Response.Write(AnSupplier.SupplierID);
+        //display Supplier name
+        Response.Write(AnSupplier.SupplierName);
+        //display Production Date
+        Response.Write(AnSupplier.ProductionDate);
+        //display ExpiryDate
+        Response.Write(AnSupplier.ExpiryDate);
+        //display Quantity
+        Response.Write(AnSupplier.Quantity);
+        //display price
+        Response.Write(AnSupplier.Price);
+
 
     }
+    
 
     protected void btnDelete_Click(object sender, EventArgs e)
     {
