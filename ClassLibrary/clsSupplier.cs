2f508b592036d3dee0235af3eb0239c19bfd8d6d
p2786795 <p2786795@my365.dmu.ac.uk>
2024-05-28 02:22:43 +0100

    testing Ok

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
+        public bool Shipmentstatus { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ExpiryDate { get; set; }
     }
 }
--- Testing5/tstSupplier.cs
+++ Testing5/tstSupplier.cs
@@ -18,18 +18,93 @@ namespace Testing5
         }
         [TestMethod]
         public void ShipmentstatusPropertyOK() 
-        {// create an instance of the class we want to create
+        {
+            // create an instance of the class we want to create
             clsSupplier AnSupplier = new clsSupplier();
             //creating test data to assign to the property
             Boolean TestData = true;
             //assign the data to property
-            AnSupplier.Shipmentstatus = TestData;
+           AnSupplier.Shipmentstatus = TestData;
             //test to see that two values are the same
             Assert.AreEqual(AnSupplier.Shipmentstatus, TestData);
 
         }
+        [TestMethod]
+        public void ProductionDatePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //creating test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
 
-        
+            //assign the data to the property
+            AnSupplier.ProductionDate = TestData;
+            //test to see that two values are same
+            Assert.AreEqual(AnSupplier.ProductionDate, TestData);
+        }
+        [TestMethod]
+        public void SupplierIDPropertyOK()
+        {  //create an instance of the class we want
+           clsSupplier AnSupplier = new clsSupplier();
+            //create test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the proverty
+            AnSupplier.SupplierID= TestData;
+            //test to see that two values are same
+            Assert.AreEqual(AnSupplier.SupplierID, TestData);
+        }
+        [TestMethod]
+        public void SupplierNamePropertyOK()
+        {
+            //CREATE AN INSTANCE OF THE CLASS WE WANT
+            clsSupplier AnSupplier = new clsSupplier();
+            //create test data to assign to the property
+            string TestData = "Some Supplier";
+            //assign the data to property
+            AnSupplier.SupplierName = TestData;
+            //test to see that two values are same
+            Assert.AreEqual(AnSupplier.SupplierName, TestData);
+
+        }
+        [TestMethod]
+        public void QuantityPropertyOK()
+        {
+            //create an instance of the class you want
+            clsSupplier AnSupplier = new clsSupplier();
+            //create test data to assign the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnSupplier.Quantity= TestData;
+            //test to see the two values are same
+            Assert.AreEqual(AnSupplier.Quantity, TestData);
+        }
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            //create an instance of the class you want
+            clsSupplier AnSupplier = new clsSupplier();
+            //create test data to assign the property
+            decimal TestData= 1.00M;
+            //assign the data to the property
+            AnSupplier.Price = TestData;
+            //test to see the two values are same
+            Assert.AreEqual(AnSupplier.Price, TestData);
+        }
+        [TestMethod]
+        public void ExpiryDateOK()
+        {  
+            //create an instance of the class you want
+            clsSupplier AnSupplier = new clsSupplier();
+            //create test data to assign thae proprty
+            DateTime TestData = new DateTime();
+            TestData.AddYears(+2);
+
+            //assign the data to the property
+            AnSupplier.ExpiryDate=TestData;
+            //test to see two values are same
+            Assert.AreEqual(AnSupplier.ExpiryDate, TestData);
+                }
+
     }
 }
 
