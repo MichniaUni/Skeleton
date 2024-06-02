32efe75b86fc848049e7031482fd79ad86908175
p2786795 <p2786795@my365.dmu.ac.uk>
2024-06-02 21:09:22 +0100

    all checked

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -8,53 +8,59 @@ namespace ClassLibrary
         //private data member for Shipmentstatus property
         private Boolean mShipmentstatus;
         //Shipmentstatus public property
-    
+
         public bool Shipmentstatus
-    
-    {
-        get
+
         {
-            //this code sends data out of property
-            return mShipmentstatus;
-        }
-            set
-        {
-            //this line allows data into property
-            mShipmentstatus = value;
-        }
+            get
+            {
+                //this code sends data out of property
+                return mShipmentstatus;
             }
+            set
+            {
+                //this line allows data into property
+                mShipmentstatus = value;
+            }
+        }
 
         //private data member for the Production Date property
         private DateTime mProductionDate;
         //ProductionDate public property
 
-        public DateTime ProductionDate {
+        public DateTime ProductionDate
+        {
             get
             { //this line code sends data out of the property
-                return mProductionDate; }
+                return mProductionDate;
+            }
 
 
             set
             {
                 //this code allows data into property
-                mProductionDate = value; }
-                }
+                mProductionDate = value;
+            }
+        }
 
         //private data member for the SupplierID Property
         private Int32 mSupplierID;
         //SupplierID public property
 
-        public Int32  SupplierID {
+        public Int32 SupplierID
+        {
             get
             { // this line of code sends data out of the property
 
-                return mSupplierID; }
+                return mSupplierID;
+            }
 
-                set
-            { 
+            set
+            {
                 //this line allows data into the property
-                mSupplierID = value; }
-                }
+                mSupplierID = value;
+            }
+        }
 
 
         //private data member for the Supplier name property
@@ -67,9 +73,9 @@ namespace ClassLibrary
                 //this code sends data out of the property
                 return mSupplierName;
             }
-                
-                set
-            { 
+
+            set
+            {
                 //this code allows data into the property
                 mSupplierName = value;
             }
@@ -78,7 +84,7 @@ namespace ClassLibrary
         //private data member for Quantity Proverty
         private Int32 mQuantity;
         //Quantity public property
-        public int Quantity 
+        public int Quantity
         {
             get
             {
@@ -92,12 +98,25 @@ namespace ClassLibrary
                 mQuantity = value;
             }
         }
-        public decimal Price { get; set; }
+        //private data member for Price Poperty
+        private Int32 mPrice;
+
+        //Price oublic poperty
+        public decimal Price {
+        get
+            { // this code allows data into the property
+                return mPrice;
+            }
+            set
+            {// this code allows data into the property
+                mPrice = value;
+            }
+        }
 
         // private data member for Expiry date property
         private DateTime mExpiryDate;
         //ExpiryDate public property
-        public DateTime ExpiryDate 
+        public DateTime ExpiryDate
         {
             get
             {
@@ -114,16 +133,31 @@ namespace ClassLibrary
 
         public bool Find(int supplierID)
         {
-            //set the private data members to the test data value
-            mSupplierID = 3;
-            mProductionDate = Convert.ToDateTime("06/12/2002");
-            mSupplierName = "Test SupplierName";
-            mShipmentstatus=true;
-            mQuantity = 10;
-            mExpiryDate = Convert.ToDateTime("06/12/2004");
-            //always return true
-            return true;
-
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //addparameter for the Supplier ID to search for
+            DB.AddParameter("@SupplierID", SupplierID);
+            //excute the stored procedure
+            DB.Execute("sproc_tblSupplier_FilterBySupplierID");
+            //if one record is found
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mSupplierID = Convert.ToInt32(DB.DataTable.Rows[0]["SupplierID"]);
+                mProductionDate = Convert.ToDateTime(DB.DataTable.Rows[0]["ProductionDate"]);
+                mSupplierName = Convert.ToString(DB.DataTable.Rows[0]["SupplierName"]);
+                mShipmentstatus = Convert.ToBoolean(DB.DataTable.Rows[0]["Shipmentstatus"]);
+                mQuantity = Convert.ToInt32(DB.DataTable.Rows[0]["Quantity"]);
+                mExpiryDate = Convert.ToDateTime(DB.DataTable.Rows[0]["ExiryDate"]);
+                //return that everything worked on
+                return true;
+            }
+            //if no recrds found
+            else
+            {
+                //return false indicating there is problem
+                return false;
+            }
         }
     }
 }
