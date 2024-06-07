6468f20d496985e916856854d18f695fdaeb848c
p2785933 <p2785933@my365.dmu.ac.uk>
2024-06-07 04:48:44 +0100

    asdad

--- AdminSystem/ShipmentManagementList.aspx.cs
+++ AdminSystem/ShipmentManagementList.aspx.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if this is the first time apge displyed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayShipments();
+        }
+    }
 
+    void DisplayShipments()
+    {
+        //Create an instance of the shpment collection
+        clsShipmentCollection Shipments = new clsShipmentCollection();
+        //set the data sources to list of addresses in th ecollection
+        lstshipmentlist.DataSource = Shipments.ShipmentList;
+        //SET THE NAME OF PRIMARY KEY
+        lstshipmentlist.DataValueField = "shipmentid";
+        //set the data firld to display
+        lstshipmentlist.DataTextField = "postcode";
+        //bind the data to the list
+        lstshipmentlist.DataBind();
     }
 }
---
+++ ClassLibrary/clsShipmentCollection.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ClassLibrary
+{
+    public class clsShipmentCollection
+    {
+        public clsShipmentCollection()
+        {
+            //variable for the index
+            Int32 Index = 0;
+            //variable to store record count
+            Int32 RecordCount = 0;
+            //data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblShipment_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                //create a blank shipment
+                clsShipment AnShipment = new clsShipment(); 
+                AnShipment.isDeliveryExpress = Convert.ToBoolean(DB.DataTable.Rows[Index]["isDeliveryExpress"]);
+                AnShipment.street = Convert.ToString(DB.DataTable.Rows[Index]["street"]);
+                AnShipment.city = Convert.ToString(DB.DataTable.Rows[Index]["city"]);
+                AnShipment.postcode = Convert.ToString(DB.DataTable.Rows[Index]["postcode"]);
+                AnShipment.shipmentid = Convert.ToInt32(DB.DataTable.Rows[Index]["shipmentid"]);
+                AnShipment.ordernum = Convert.ToInt32(DB.DataTable.Rows[Index]["ordernum"]);
+                AnShipment.deliverydate = Convert.ToDateTime(DB.DataTable.Rows[Index]["deliverydate"]);
+                mShipmentList.Add(AnShipment);
+                //point for next record
+                Index++;
+
+
+            }
+            
+          
+        }
+
+
+       
+
+        //private data member for the list
+        List<clsShipment> mShipmentList = new List<clsShipment>();
+
+
+
+        public List<clsShipment> ShipmentList
+        {
+            get
+            {
+                //return the private data
+                return mShipmentList;
+            }
+            set
+            {
+                mShipmentList = value;
+            }
+        }
+
+             
+        public int Count
+        {
+            get
+            {
+                return mShipmentList.Count;
+            }
+            set
+            {
+                //laterrr
+            }
+        }
+        public clsShipment ThisShipment { get; set; }
+    }
+}
